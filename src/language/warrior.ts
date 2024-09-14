37b61c9dfddb56fd2c816427976c8c162cbd575a
Forest Belton <forest@homolo.gy>
2024-09-13 21:26:12 -0700

    Add runtime warrior abstraction

--- src/language/warrior.ts
+++ src/language/warrior.ts
@@ -11,3 +11,46 @@ export type Warrior = {
   };
   startIndex: number;
 };
+
+export type TaskID = number;
+export type InstructionPointer = number;
+
+export type Task = {
+  taskID: TaskID;
+  instructionPointer: InstructionPointer;
+};
+
+export class DeadWarriorError extends Error {}
+
+export class VmWarrior {
+  nextID: TaskID;
+  taskQueue: Task[];
+
+  constructor(entryPoint: InstructionPointer) {
+    this.nextID = 0;
+    this.taskQueue = [];
+    this.createTask(entryPoint);
+  }
+
+  createTask(entryPoint: InstructionPointer) {
+    const task = { taskID: this.nextID++, instructionPointer: entryPoint };
+    this.taskQueue.push(task);
+  }
+
+  executeTask(f: (pc: InstructionPointer) => InstructionPointer | null) {
+    const task = this.taskQueue.shift();
+    if (typeof task === "undefined") {
+      throw new DeadWarriorError();
+    }
+
+    const nextInstructionPointer = f(task.instructionPointer);
+    if (nextInstructionPointer !== null) {
+      task.instructionPointer = nextInstructionPointer;
+      this.taskQueue.push(task);
+    }
+  }
+
+  dead(): boolean {
+    return this.taskQueue.length === 0;
+  }
+}
