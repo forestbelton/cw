09a17285f7ffda402fa345b5ba19d6981b927298
Forest Belton <forest@homolo.gy>
2024-09-21 13:04:33 -0700

    Add tests for DIV instruction

--- src/language/vm.test.ts
+++ src/language/vm.test.ts
@@ -160,6 +160,145 @@ describe("VM", () => {
         ],
       ])("ADD.%s", basicInsnTest);
     });
+    describe("DIV", () => {
+      it("DIV.A", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+            DIV.A $1, $2
+            DAT.F #222, #0
+            DAT.F #666, #0
+        `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #0"));
+        vm.core[1].a.value = 0;
+        expect(vm.executeStep(0)).toStrictEqual({});
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #0"));
+      });
+      it("DIV.B", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.B $1, $2
+              DAT.F #0, #222
+              DAT.F #0, #666
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #0, #3"));
+        vm.core[1].b.value = 0;
+        expect(vm.executeStep(0)).toStrictEqual({});
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #0, #3"));
+      });
+      it("DIV.AB", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.AB $1, $2
+              DAT.F #222, #0
+              DAT.F #0, #666
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #0, #3"));
+        vm.core[1].a.value = 0;
+        expect(vm.executeStep(0)).toStrictEqual({});
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #0, #3"));
+      });
+      it("DIV.BA", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.BA $1, $2
+              DAT.F #0, #222
+              DAT.F #666, #0
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #0"));
+        vm.core[1].b.value = 0;
+        expect(vm.executeStep(0)).toStrictEqual({});
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #0"));
+      });
+      it("DIV.F", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.F $1, $2
+              DAT.F #222, #222
+              DAT.F #666, #444
+
+              DIV.F $1, $2
+              DAT.F #222, #0
+              DAT.F #666, #444
+
+              DIV.F $1, $2
+              DAT.F #0, #222
+              DAT.F #666, #444
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #2"));
+
+        expect(vm.executeStep(3)).toStrictEqual({});
+        expect(vm.core[5]).toStrictEqual(assembleInstruction("DAT.F #3, #444"));
+
+        expect(vm.executeStep(6)).toStrictEqual({});
+        expect(vm.core[8]).toStrictEqual(
+          assembleInstruction("DAT.F #666, #444")
+        );
+      });
+      it("DIV.X", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.X $1, $2
+              DAT.F #222, #222
+              DAT.F #666, #444
+
+              DIV.X $1, $2
+              DAT.F #222, #0
+              DAT.F #666, #444
+
+              DIV.X $1, $2
+              DAT.F #0, #222
+              DAT.F #666, #444
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #2"));
+
+        expect(vm.executeStep(3)).toStrictEqual({});
+        expect(vm.core[5]).toStrictEqual(assembleInstruction("DAT.F #666, #2"));
+
+        expect(vm.executeStep(6)).toStrictEqual({});
+        expect(vm.core[8]).toStrictEqual(
+          assembleInstruction("DAT.F #666, #444")
+        );
+      });
+      it("DIV.I", () => {
+        const vm = VM.create(icw86Options, [
+          assemble(`
+              DIV.I $1, $2
+              DAT.F #222, #222
+              DAT.F #666, #666
+
+              DIV.I $1, $2
+              DAT.F #222, #0
+              DAT.F #666, #666
+
+              DIV.I $1, $2
+              DAT.F #0, #222
+              DAT.F #666, #666
+          `),
+        ]);
+        expect(vm.executeStep(0)).toStrictEqual({ nextPointer: 1 });
+        expect(vm.core[2]).toStrictEqual(assembleInstruction("DAT.F #3, #3"));
+
+        expect(vm.executeStep(3)).toStrictEqual({});
+        expect(vm.core[5]).toStrictEqual(assembleInstruction("DAT.F #3, #666"));
+
+        expect(vm.executeStep(6)).toStrictEqual({});
+        expect(vm.core[8]).toStrictEqual(
+          assembleInstruction("DAT.F #666, #666")
+        );
+      });
+    });
     describe("JMP", () => {
       it("performs relative jump to A-pointer", () => {
         const vm = VM.create(icw86Options, [
